b407860a7f5c2b5e5fe20af92a5e585ec29402ba
Linas <linas404@protonmail.com>
2025-04-05 21:14:32 +0200

    Initial Commit

---
+++ CustomRating/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomRating
+//
+//  Created by Linas on 05/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomRating/CustomRatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRatingApp.swift
+//  CustomRating
+//
+//  Created by Linas on 05/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
